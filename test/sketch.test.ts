51c6721abe93976468333d57ca2fc73c14724806
friaca <v.fria@outlook.com>
2020-03-08 19:56:54 -0300

    add: testes unitários `sketch`

--- test/sketch.test.ts
+++ test/sketch.test.ts
@@ -26,5 +26,37 @@ test('retorna o sketch', () => {
     version: PVersions.p3,
   }
 
-  expect(getSketch('pixelSort')).toBe(expected)
+  expect(getSketch('pixelSort')).toStrictEqual(expected)
+})
+
+test('retorna o comando certo pra executar o script', () => {
+  const regex = /^.+-(\d.\d.\d).+\.exe --sketch=.+(\\|\/)(\w+) --run$/gm;
+  const cmd = getProcessingCmd('pixelSort');
+  const matches = cmd.match(regex);
+
+  expect(matches).toBeTruthy();
+})
+
+test('retorna o caminho da pasta de recursos (fontes)', () => {
+  const regex = /(\\|\/)assets$/gm;
+  const pathAssets = getAssetsPath('pixelSort');
+  const matches = pathAssets.match(regex);
+
+  expect(matches).toBeTruthy();
+})
+
+test('retorna o caminho da pasta de arquivos necessários pro script funcionar', () => {
+  const regex = /(\\|\/)data$/gm;
+  const pathData = getDataPath('pixelSort');
+  const matches = pathData.match(regex);
+
+  expect(matches).toBeTruthy();
+})
+
+test('retorna o caminho da pasta do arquivo de preparação do script', () => {
+  const regex = /(\\|\/)setup.txt$/gm;
+  const pathSetup = getSetupFilePath('pixelSort');
+  const matches = pathSetup.match(regex);
+
+  expect(matches).toBeTruthy();
 })
