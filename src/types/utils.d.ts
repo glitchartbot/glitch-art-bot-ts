01a7d81be01c4b2d77fda76ea6325da0984747e6
friaca <v.fria@outlook.com>
2020-03-11 22:55:15 -0300

    add: pasta pros tipos de resposta da api

--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -1,11 +1,5 @@
 import { LogEntry } from 'winston'
 
-export enum StepEnum {
-  GET_TWEET,
-  DOWNLOAD_IMAGE,
-  REPLY_TWEET
-}
-
 export interface IFile {
   name: string,
   format: string
@@ -15,6 +9,6 @@ export interface ILog extends LogEntry {
   id?: string
 }
 
-export interface ExtendedError extends Error {
-  step: StepEnum
+interface ExtendedError extends Error {
+  id?: string
 }
