4cf2237f3c0c0ab4330cc1cd528ae4da17a0cfd3
friaca <v.fria@outlook.com>
2020-03-14 18:40:34 -0300

    fix: types

--- src/bot.ts
+++ src/bot.ts
@@ -5,13 +5,14 @@ import { keys } from './config';
 import * as utils from './util/utils';
 
 import { Get } from './types/twit';
+import { Params } from './types/api';
 import { Tweet } from './types/twitter/tweet';
 import { UploadedMedia } from './types/twitter/media';
 import { SketchOption } from './types/sketch';
 import { ExtendedError, IFile } from './types/utils';
 
 const twitter = new twit(keys);
-const ID = '1232403291151196160';
+export const ID = '1232403291151196160';
 
 function uploadTweet(params: any): Promise<Tweet> { 
   return twitter.post('statuses/update', params)
@@ -22,14 +23,15 @@ function uploadTweet(params: any): Promise<Tweet> {
 function uploadImage(sketch: SketchOption, file: IFile): Promise<UploadedMedia> {
   const filePath = utils.getFilePath(sketch, file);
   const b64 = readFileSync(filePath, { encoding: 'base64' });
+  const params: Params = { media_data: b64 };
 
-  return twitter.post('media/upload', { media_data: b64 })
+  return twitter.post('media/upload', params)
     .then((res: Get<UploadedMedia>) => res.data)
     .catch((error: ExtendedError) => { throw error; })
 }
 
 export function getTweetById(tweetId: string): Promise<Tweet> {
-  const params = { id: tweetId, tweet_mode: 'extended' }
+  const params: Params = { id: tweetId, tweet_mode: 'extended' }
 
   return twitter.get('statuses/show', params)
     .then((res: Get<Tweet>) => res.data)
@@ -45,13 +47,14 @@ export function listenQuery(query: string | string[], callback: Function) {
 }
 
 export function replyTweet(tweetId: string): Promise<Tweet>
+export function replyTweet(tweetId: string, status?: string, sketch?: SketchOption, file?: IFile): Promise<Tweet>
 
 export async function replyTweet(tweetId: string, status?: string, sketch?: SketchOption, file?: IFile): Promise<Tweet> {
   try {
     const tweet = await getTweetById(tweetId);
     const screenName = tweet.user.screen_name;
     let uploaded: UploadedMedia;
-    let params: any = {
+    let params: Params = {
       in_reply_to_status_id: tweet.id_str, 
       status: `@${screenName}` 
     };
