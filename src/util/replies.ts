db3e2e6e70ee3e234a0f19f4ee99dbc6eaa3f32b
friaca <v.fria@outlook.com>
2020-04-19 20:42:11 -0300

    [add] more replies

--- src/util/replies.ts
+++ src/util/replies.ts
@@ -1,5 +1,7 @@
 export default {
   defaultConfig: 'No valid configuration found, using default config.\nFor more information on using custom options, visit {{url}}',
   invalidSketch: 'No sketch found with this name, visit {{url}} for available sketches',
-  invalidImage: 'No valid image found in the parent tweet.'
+  invalidImage: 'No valid image found in the parent tweet',
+  orphanTweet: 'No parent tweet found, for more information on the bot usage, vitit {{url}}',
+  standard: 'Here is your edited image :)'
 }
