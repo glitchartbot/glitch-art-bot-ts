7e6a5f44333bf9d3e05e499061b00795495a2c32
friaca <v.fria@outlook.com>
2020-04-20 22:49:30 -0300

    [fix] placeholder url in replies

--- src/util/replies.ts
+++ src/util/replies.ts
@@ -1,7 +1,7 @@
 export default {
-  defaultConfig: 'No valid configuration found, using default config.\nFor more information on using custom options, visit {{url}}',
-  invalidSketch: 'No sketch found with this name, visit {{url}} for available sketches',
+  defaultConfig: 'No valid configuration found, using default config.\nFor more information on using custom options, visit https://github.com/friaca/glitch-art-bot-scripts',
+  invalidSketch: 'No sketch found with this name, visit https://github.com/friaca/glitch-art-bot-scripts for available sketches',
   invalidImage: 'No valid image found in the parent tweet',
-  orphanTweet: 'No parent tweet found, for more information on the bot usage, vitit {{url}}',
+  orphanTweet: 'No parent tweet found, for more information on the bot usage, vitit https://github.com/friaca/glitch-art-bot-scripts',
   standard: 'Here is your edited image :)'
 }
