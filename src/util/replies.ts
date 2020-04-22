3e5b00d72c6ce87fb29850fdba1c29bae8f02739
friaca <v.fria@outlook.com>
2020-04-22 20:26:41 -0300

    [fix] spelling error

--- src/util/replies.ts
+++ src/util/replies.ts
@@ -2,6 +2,6 @@ export default {
   defaultConfig: 'No valid configuration found, using default config.\nFor more information on using custom options, visit https://github.com/friaca/glitch-art-bot-scripts',
   invalidSketch: 'No sketch found with this name, visit https://github.com/friaca/glitch-art-bot-scripts for available sketches',
   invalidImage: 'No valid image found in the parent tweet',
-  orphanTweet: 'No parent tweet found, for more information on the bot usage, vitit https://github.com/friaca/glitch-art-bot-scripts',
+  orphanTweet: 'No parent tweet found, for more information on the bot usage, visit https://github.com/friaca/glitch-art-bot-scripts',
   standard: 'Here is your edited image :)'
 }
