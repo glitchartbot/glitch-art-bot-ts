f147d737b3010cc95ad77685df5d004b2a9efd2e
friaca <v.fria@outlook.com>
2020-04-26 22:53:39 -0300

    [change] toc

--- src/util/replies.ts
+++ src/util/replies.ts
@@ -1,10 +1,10 @@
 export default {
+  standard: 'Here is your edited image :)',
+  invalidImage: 'No valid image found in the parent tweet',
   defaultConfig:
     'No valid configuration found, using default config.\nFor more information on using custom options, visit https://github.com/friaca/glitch-art-bot-scripts',
   invalidSketch:
     'No sketch found with this name, visit https://github.com/friaca/glitch-art-bot-scripts for available sketches',
-  invalidImage: 'No valid image found in the parent tweet',
   orphanTweet:
     'No parent tweet found, for more information on the bot usage, visit https://github.com/friaca/glitch-art-bot-scripts',
-  standard: 'Here is your edited image :)',
 };
